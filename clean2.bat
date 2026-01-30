aef6ddcb35228ec881dafaed8a568ebc762262f3
TONYPC\tony <shengdongtan@gmail.com>
2026-01-30 12:37:02 +0800

    Remove clean.bat

--- clean2.bat
+++ clean2.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "C:\Users\tony\clawd\minijam_flow"
-del done.bat
+del clean.bat
 git add -A
-git commit -m "Remove done.bat"
+git commit -m "Remove clean.bat"
 git push origin master
