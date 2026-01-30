2c9b7eb2a17e963fb2bb63f0f64dec913e47a371
TONYPC\tony <shengdongtan@gmail.com>
2026-01-30 12:35:54 +0800

    Cleanup temporary batch files

---
+++ cleanup_push.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\Users\tony\clawd\minijam_flow"
+git add -A
+git commit -m "Cleanup temporary batch files"
+git push origin master
--- git_commit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\tony\clawd\minijam_flow"
-git add -A
-git status
