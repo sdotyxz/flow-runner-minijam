8f5e8fe0c1b7cf030bb4ef98eb5460864b44eef7
TONYPC\tony <shengdongtan@gmail.com>
2026-01-30 15:33:28 +0800

    Add star floating animation

--- scripts/collectible.gd
+++ scripts/collectible.gd
@@ -1,11 +1,21 @@
 extends Area2D
 
 var rotation_speed = 2.0
+var move_speed = 100.0
+var move_range = 50.0
+var start_y = 0.0
+var time_offset = 0.0
 
 @onready var sprite = $Sprite2D
 
 func _ready():
 	add_to_group("collectibles")
+	start_y = position.y
+	time_offset = randf() * TAU  # 随机初始相位
 
 func _process(delta):
 	rotation += rotation_speed * delta
+	
+	# 持续上下浮动
+	time_offset += delta * move_speed * 0.01
+	position.y = start_y + sin(time_offset) * move_range
